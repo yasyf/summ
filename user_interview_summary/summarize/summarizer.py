50a74c2ba03639c5547172b67df470be520bec0d
Yasyf Mohamedali <yasyfm@gmail.com>
2023-01-21 12:50:33 -0800

    Add Summarizer.

--- user_interview_summary/summarize/summarizer.py
+++ user_interview_summary/summarize/summarizer.py
@@ -1,2 +1,13 @@
-class Summarizer:
-    pass
+from langchain.chains.summarize import load_summarize_chain
+
+from user_interview_summary.shared.chain import Chain
+
+
+class Summarizer(Chain):
+    def summarize_class(self, docs):
+        chain = load_summarize_chain(self.llm, chain_type="map_reduce")
+        return chain.run(docs)
+
+    def summarize_file(self, docs):
+        chain = load_summarize_chain(self.llm, chain_type="refine")
+        return chain.run(docs)

52fc9427704d9a5420ca519234008eb3cc75fe60
Yasyf Mohamedali <yasyfm@gmail.com>
2023-01-21 11:28:29 -0800

    Accept docs.

--- user_interview_summary/classify/classifier.py
+++ user_interview_summary/classify/classifier.py
@@ -1,14 +1,22 @@
-from user_interview_summary.classify.classes import Classes
-from langchain import PromptTemplate, FewShotPromptTemplate
 from textwrap import dedent
 
+from langchain import FewShotPromptTemplate, PromptTemplate
+from langchain.docstore.document import Document
+
+from user_interview_summary.classify.classes import Classes
+
+
 class Classifier:
-    def classify(self, title: str, interview: str) -> list[Classes]:
-        
+    def classify(self, doc: Document) -> list[Classes]:
+        text, title = doc.page_content, doc.metadata["file"]
+
         # First, create the list of few shot examples.
         examples = [
             {"title": "Brendan - UiPath - Sales", "department": "Sales"},
-            {"title": "Amy - Blue Prism - Lead Automation Developer", "department": "RPA Development"},
+            {
+                "title": "Amy - Blue Prism - Lead Automation Developer",
+                "department": "RPA Development",
+            },
         ]
 
         # Next, we specify the template to format the examples we have provided.
@@ -23,14 +31,15 @@ class Classifier:
         )
 
         # Finally, we create the `FewShotPromptTemplate` object.
-        few_shot_prompt = FewShotPromptTemplate( 
+        few_shot_prompt = FewShotPromptTemplate(
             # These are the examples we want to insert into the prompt.
             examples=examples,
             # This is how we waånt to format the examples when we insert them into the prompt.
             example_prompt=example_prompt,
             # The prefix is some text that goes before the examples in the prompt.
             # Usually, this consists of intructions.
-            prefix=dedent("""
+            prefix=dedent(
+                """
             The following is a title of a user interview. The user interviewed works in one or more of the following departments:
             Sales
             Finance
@@ -47,8 +56,9 @@ class Classifier:
             RPA Developement
             Customer Success
 
-            Return only a list of departments, no explanation. For example: "Operations, Sales" if the user works in both Operations and Sales. The form of the title is "Name - Name of Company - Role". Note if a user works at a company that builds RPA software or is an RPA consultancy, they do not work at the RPA Center of Excellence, it means they are an RPA developer to external customers.         
-            """),
+            Return only a list of departments, no explanation. For example: "Operations, Sales" if the user works in both Operations and Sales. The form of the title is "Name - Name of Company - Role". Note if a user works at a company that builds RPA software or is an RPA consultancy, they do not work at the RPA Center of Excellence, it means they are an RPA developer to external customers.
+            """
+            ),
             # The suffix is some text that goes after the examples in the prompt.
             # Usually, this is where the user input will go
             suffix="User Interview Title: {input}\nDepartment:",
@@ -59,7 +69,11 @@ class Classifier:
         )
 
         # We can now generate a prompt using the `format` method.
-        print(few_shot_prompt.format(input="Ricardo - Automation Anywhere - Customer Success"))
+        print(
+            few_shot_prompt.format(
+                input="Ricardo - Automation Anywhere - Customer Success"
+            )
+        )
         # The following is a title of a user interview. The user interviewed works in one or more of the following departments:
         # Sales
         # Finance
@@ -80,7 +94,6 @@ class Classifier:
 
         # User Interview Title: Brendan - UiPath - Sales
         # Department Classification: Sales
-
 
         # User Interview Title: Amy - Blue Prism - Lead Automation Developer
         # Department Classification: RPA Development

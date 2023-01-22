01c251f60f522cdb8fb96657cf6bde69b414f6ef
Yasyf Mohamedali <yasyfm@gmail.com>
2023-01-21 18:55:16 -0800

    Add sample examples.

--- user_interview_summary/query/querier.py
+++ user_interview_summary/query/querier.py
@@ -1,5 +1,4 @@
 import itertools
-from textwrap import dedent
 from typing import cast
 
 import metrohash
@@ -10,32 +9,54 @@ from langchain.embeddings import OpenAIEmbeddings
 from user_interview_summary.classify.classes import Classes
 from user_interview_summary.embed.embedder import Embedding
 from user_interview_summary.shared.chain import Chain
+from user_interview_summary.shared.utils import dedent
 
 
 class Querier(Chain):
     INDEX = "rpa-user-interviews"
+    EXAMPLES = [
+        {
+            "fact": "I like to play video games",
+            "attributes": ["gamer"],
+            "context": "I like to play video games",
+        },
+    ]
+    EXAMPLE_PROMPT = PromptTemplate(
+        input_variables=["fact", "context", "attributes"],
+        template=dedent(
+            """
+                Fact: {fact}
+                Context: {context}
+                Attributes: {attributes}
+                """
+        ),
+    )
 
     def __init__(self):
         super().__init__()
         self.embeddings = OpenAIEmbeddings()
         self.index = pinecone.Index(self.INDEX)
 
+    def examples(self):
+        return FewShotPromptTemplate(
+            examples=self.EXAMPLES,
+            example_prompt=self.EXAMPLE_PROMPT,
+            prefix="",
+            suffix="",
+            input_variables=[],
+            example_separator="\n",
+        ).format()
+
     def prompt_template(self, examples):
         return FewShotPromptTemplate(
             examples=examples,
-            example_prompt=PromptTemplate(
-                input_variables=["fact", "context", "attributes"],
-                template=dedent(
-                    """
-                Fact: {fact}
-                Context: {context}
-                Attributes: {attributes}
-                """
-                ),
-            ),
+            example_prompt=self.EXAMPLE_PROMPT,
             prefix=dedent(
                 f"""
                 The following is a list of facts, and attributes about the user who said them. Each fact comes with context and attributes that describe the user. Each set of fact, context, attributes is said by a different user, talking about their experience.
+
+                Here are some examples:
+                {self.examples()}
 
                 """
             ),
